f0e0af51144da0ed811c649261bb69f4abfe98f1
Yakukushk <daniilliubko@gmail.com>
2022-12-26 16:14:36 +0100

    Update Commit
    add
    fix movement
    if press l.shift could freeze camera
    remove
    useless components

--- Assets/Scripts/CameraPosition.cs
+++ Assets/Scripts/CameraPosition.cs
@@ -5,20 +5,34 @@ using UnityEngine;
 public class CameraPosition : MonoBehaviour
 {
     GameObject player;
-    bool isFollow = true;
+   public bool isFollow = true;
+    Camera cam;
+    movement move;
+    Vector3 mousePos;
     public void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
+        
+        cam = Camera.main;
+        move = player.GetComponent<movement>();
     }
     public void Update()
     {
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            isFollow = false;
+            move.SetMoving(false);      
+        }
+        else {
+            isFollow = true;
+        }
         if (isFollow == true)
         {
             camFollowPlayer();
-        }
+        }   
         else
         {
-            print("Error");
+            lookAround();           
             Debug.Log(gameObject.activeSelf);
         }
     }
@@ -32,5 +46,14 @@ public class CameraPosition : MonoBehaviour
             
         Vector3 newPos = new Vector3(player.transform.position.x, player.transform.position.y, this.transform.position.z);
         this.transform.position = newPos;
+    }
+    public void lookAround() {
+        Vector3 camPos = cam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y));
+        camPos.z = -10;
+        Vector3 direction = camPos - this.transform.position;
+        if (player.GetComponent<MeshFilter>().sharedMesh == true) {
+            transform.Translate(direction * 2 * Time.deltaTime);
+        }
+
     }
 }
--- Assets/Scripts/movement.cs
+++ Assets/Scripts/movement.cs
@@ -10,35 +10,51 @@ public class movement : MonoBehaviour
 
     public void Update()
     {
-        PlayerMovement();
+        if (isActiveMovement == true)
+        {
+            PlayerMovement();
+        }
+        checkMovement(isActive);
+    }
+    public void SetMoving(bool val) {
+        isActiveMovement = val;         
     }
     public void PlayerMovement() {
-     
-            if (Input.GetKey(KeyCode.W))
-            {
-                transform.Translate(Vector3.up * speed * Time.deltaTime, Space.World);
-                isActive = true;
-                Debug.Log(this.gameObject.activeSelf + "Active W");
-            }
-            if (Input.GetKey(KeyCode.A))
-            {
-                transform.Translate(Vector3.left * speed * Time.deltaTime, Space.World);
-                isActive = true;
-                Debug.Log(this.gameObject.activeSelf + "Active A");
-            }
-            if (Input.GetKey(KeyCode.D))
-            {
-                transform.Translate(Vector3.right * speed * Time.deltaTime, Space.World);
-                isActive = true;
-                Debug.Log(this.gameObject.activeSelf + "Active D");
-            }
-            if (Input.GetKey(KeyCode.S))
-            {
-                transform.Translate(Vector3.down * speed * Time.deltaTime, Space.World);
-                isActive = true;
-                Debug.Log(this.gameObject.activeSelf + "Active S");
-            }
-            
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.up * speed * Time.deltaTime, Space.World);
+            isActive = true;
+            Debug.Log(this.gameObject.activeSelf + "Active W");
         }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.left * speed * Time.deltaTime, Space.World);
+            isActive = true;
+            Debug.Log(this.gameObject.activeSelf + "Active A");
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * speed * Time.deltaTime, Space.World);
+            isActive = true;
+            Debug.Log(this.gameObject.activeSelf + "Active D");
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector3.down * speed * Time.deltaTime, Space.World);
+            isActive = true;
+            Debug.Log(this.gameObject.activeSelf + "Active S");
+        }
+
+    }
+    public void checkMovement(bool Active) {
+        if (Input.GetKey(KeyCode.D) != true && Input.GetKey(KeyCode.A) != true && Input.GetKey(KeyCode.W) != true && Input.GetKey(KeyCode.S) != true)
+        {
+            isActiveMovement = false;
+        }
+        else {
+            isActiveMovement = true;  
+        }
+    }
     
 }
